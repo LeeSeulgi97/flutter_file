fa7c2277858262e74a33e54bbf5197030b648dd7
이슬기 <dltmfrl2632@gmail.com>
2022-06-09 15:50:46 +0900

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // 리본띠
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
